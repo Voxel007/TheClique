d7f6a5a46b8774d3a910ece07b6b75c4a8f34052
Alexander August <alexander.va245@gmail.com>
2025-03-08 13:43:43 +0100

    updated linting to include reacthooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
-// import globals from 'globals'
-// import reactHooks from 'eslint-plugin-react-hooks'
-// import reactRefresh from 'eslint-plugin-react-refresh'
+import react from "eslint-plugin-react"; 
+import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
@@ -8,8 +7,20 @@ import prettier from "eslint-config-prettier";
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-
-  prettier,
   //tseslint.configs.strict,
   //tseslint.configs.stylistic,
+
+  // React & Hooks plugin configuration
+  // react.configs.recommended,
+  // reactHooks.configs.recommended,
+
+  // Flat config for plugins: define plugins as an object
+  {
+    plugins: {
+      react: react,
+      "react-hooks": reactHooks,
+    },
+  },
+
+  prettier,
 );
